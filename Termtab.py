f65e8621d16ca30e7844d3a6038049d6ae78f130
Bartender <bartender.winery@gmail.com>
2021-12-16 22:58:49 -0500

    Update Termtab.py

--- Termtab.py
+++ Termtab.py
@@ -23,12 +23,12 @@ class terminal(): #terminal super bundle
     def SLR(package): #Search lookup return
         cprint("Searching for: "+package[0], "blue")
         for index in search(package[0], tld="co.in", num=package[1], stop=package[1], pause=2): #Googlesearch
-            #try:
+            try:
                 list.append(str(index))
                 link = requests.get(index); cprint(str(len(list))+"; "+index+"; "+str(link.elapsed.total_seconds()*100),"magenta"); soup = BeautifulSoup(link.text); metas = soup.find_all('meta')
                 cprint([meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description'], "red")
-            #except:
-            #    cprint("###: "+index+" :###", "magenta")
+            except:
+                cprint("###: "+index+" :###", "magenta")
 
 ccprint(["DNS lookup: ", "Range set: "],[DNS, str(MSR)],"green") #return list
 terminal.SLR([DNS,MSR])
