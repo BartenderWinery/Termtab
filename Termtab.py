9b72edd0d4dae72a0e64e38df29d832babd4c38c
Bartender <bartender.winery@gmail.com>
2021-12-17 17:06:04 -0500

    Arguments

--- Termtab.py
+++ Termtab.py
@@ -2,12 +2,17 @@ from googlesearch import *
 from termcolor import *
 from colorama import *
 from bs4 import BeautifulSoup
-import requests, threading, colorama, webbrowser
+import requests, threading, colorama, webbrowser, argparse
 
 colorama.init()
 
-DNS = input(Fore.BLUE+"DNS: ")
-MSR = int(input(Fore.BLUE+"Max range: "))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A quick term indexing tool to quickly find websites related to your keywords.")
+    parser.add_argument('DNS', metavar='DNS', action='store', help='Domain term search')
+    parser.add_argument('--MSR', metavar='MSR', type=int, help='Maximum search range', default=10)
+    parser.add_argument("--Threads", metavar='Threads', type=int, help='Threads to use', default=1)
+    parser.add_argument("--PDR", metavar="PDR", action='store', help='Prefered domain for searches')
+    args = parser.parse_args()
 
 list = []
 #Add threading; thread control semi-complete: no dividing done
@@ -15,28 +20,27 @@ list = []
 def ccprint(respo,printpack,color): #mass print
     for pack in range(len(printpack)):
         cprint(respo[pack]+printpack[pack],color)
-def thread(RTP): #Requested Threading profiling; simple control to start defs
-    for thread in range(len(RTP)):
-        cprint("Threading: "+RTP[thread], "blue")
-        RTP[thread].start()
 class terminal(): #terminal super bundle
     def SLR(package): #Search lookup return
         cprint("Searching for: "+package[0], "blue")
         for index in search(package[0], tld="co.in", num=package[1], stop=package[1], pause=2): #Googlesearch
             try:
                 list.append(str(index))
-                link = requests.get(index); cprint(str(len(list))+"; "+index+"; "+str(link.elapsed.total_seconds()*100),"magenta"); soup = BeautifulSoup(link.text); metas = soup.find_all('meta')
+                link = requests.get(index); cprint(str(len(list))+"; "+index+"; "+str(link.elapsed.total_seconds()*100),"magenta"); soup = BeautifulSoup(link.text, features="lxml"); metas = soup.find_all('meta')
                 cprint([meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description'], "red")
             except:
                 cprint("###: "+index+" :###", "magenta")
+    def thread(RTP): #Requested Threading profiling; simple control to start defs
+        for thread in range(len(RTP)):
+            cprint("Threading: "+RTP[thread], "blue")
+            RTP[thread].start()
 
-ccprint(["DNS lookup: ", "Range set: "],[DNS, str(MSR)],"green") #return list
-terminal.SLR([DNS,MSR])
+ccprint(["DNS lookup: ", "Range set: "],[args.DNS, str(args.MSR)],"green") #return list
+terminal.SLR([args.DNS,args.MSR])
 
-web = int(input(Fore.BLUE+"Open: "))
-
+web = input(Fore.BLUE+"Open: ")
 try:
-    cprint("Opening: "+str(list[web]), "green")
+    cprint("Opening: "+str(list[int(web)]), "green")
     webbrowser.open_new(str(list[web]))
 except:
     cprint("...")
